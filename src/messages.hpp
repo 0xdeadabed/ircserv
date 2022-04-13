bbe7a793feae260eb4def18a9884dbaab6475f22
hypn0x <leetpwnsroot@gmail.com>
2022-04-13 17:52:02 +0200

    Fixed for the last test case

--- src/messages.hpp
+++ src/messages.hpp
@@ -7,9 +7,9 @@
 #define IRCSERV_MESSAGES_HPP
 
 #define RPL_JOIN(client, channel)				":" + client + " JOINED " + channel + "\r\n"
-#define ERR_NONICKNAMEGIVEN						"431 * :No nickname given\r\n"
+#define ERR_NONICKNAMEGIVEN						"431 :No nickname given\r\n"
 #define ERR_NICKNAMEINUSE(nick)					"433 * " + nick + " :Nickname is already in use\r\n"
-#define RPL_WELCOME(nick, user)			"001 * :Welcome to the Internet Relay Network " + nick + "!" + user + "@42.lausanne.ch\r\n"
+#define RPL_WELCOME(nick, user)					"001 * :Welcome to the Internet Relay Network " + nick + "!" + user + "@42.lausanne.ch\r\n"
 #define ERR_ALREADYREGISTERED					"462 :Unauthorized command (already registered)\r\n"
 #define	ERR_NEEDMOREPARAMS(cmd)					"461 " cmd " :Not enough parameters\r\n"
 #define	ERR_PASSWDMISMATCH						"464 * :Password incorrect\r\n"
